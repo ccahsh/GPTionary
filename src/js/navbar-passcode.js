ffb6057d088194ddd4f65c94d3771b4cd1a1e93b
Charles Shin <30178389+tlsgusdn1107@users.noreply.github.com>
2023-02-12 07:43:29 -0500

    Update navbar-passcode.js

--- src/js/navbar-passcode.js
+++ src/js/navbar-passcode.js
@@ -17,6 +17,10 @@ $(document).ready(function () {
 	};
 
 	var password = getCookie("password");
-	document.getElementById("passcode").innerHTML = password;
-	document.getElementById("passcode1").innerHTML = password;
+	
+	document.getElementById("passcode").innerHTML = "";
+	document.getElementById("passcode1").innerHTML = "";
+// 	COMMENT THE LINES ABOVE AND UNCOMMENT THE LINES BELOW ONCE BETA (FREE) RELEASE IS OVER
+// 	document.getElementById("passcode").innerHTML = password;
+// 	document.getElementById("passcode1").innerHTML = password;
 });
